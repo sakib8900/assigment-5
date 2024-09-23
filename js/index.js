19d7eef931276f7291b83c12ed352ad533a7c7c0
sakib8900 <md.sakib388900@gmail.com>
2024-09-24 00:52:14 +0600

    dom related some kaj

--- js/index.js
+++ js/index.js
@@ -1,3 +1,22 @@
+// get value
+function getInputValueById(id){
+    return parseFloat(document.getElementById(id).value);
+}
+// blog html jawar jnno code
 const blogContainer = document.getElementById("btn-blog").addEventListener('click', function(){
     window.location.href = './blog.html'
 });
+
+// work str
+document.getElementById("btn-noakhali").addEventListener('click', function(){
+    // input theka man
+    const noakhaliAmount = getInputValueById("noakhali-amnt");
+    const totalAmount = parseFloat(document.getElementById("total-amount").innerText);
+    // 1st input span
+    const addedTk = document.getElementById("added-tk");
+    addedTk.innerText = noakhaliAmount;
+
+    const baciJawaTk = totalAmount - noakhaliAmount;
+    const addedBacaTk = document.getElementById("total-amount");
+    addedBacaTk.innerText = baciJawaTk;
+})
